b778dbc69559cad4a93dc61c4f0252e594dc59d9
Jessica Sachs <jess@jessicasachs.io>
2020-09-05 21:07:31 -0400

    Broken cells

--- src/components/Cell.vue
+++ src/components/Cell.vue
@@ -1,8 +1,8 @@
 <template>
-  <span class="cell" data-testid="cell">
+  <span class="cell" data-testid="cell" :class="classes">
     {{ letter }}
     <span class="number" data-testid="cell-number">
-      {{ number }}
+      {{ number !== 0 ? number : '' }}
     </span>
   </span>
 </template>
@@ -12,6 +12,11 @@
     props: {
       letter: { type: String, default: '' },
       number: { type: [String, Number], optional: true , default: ''},
+    },
+    computed: {
+      classes() {
+        return !this.letter ? 'blocked-out' : ''
+      }
     }
   }
 </script>
@@ -19,16 +24,32 @@
 <style lang="scss" scoped>
   .cell {
     position: relative;
-    width: 1.25rem;
-    height: 1.25rem;
+
+    display: inline-flex;
+    align-items: center;
+    justify-content: center;
+
+    width: 3rem;
+    height: 3rem;
+    padding: 0.25rem;
+
     font-size: 1rem;
+
+    border: 1px solid black;
+
+    &.blocked-out {
+      background: black;
+    }
 
     .number {
       position: absolute;
-      color: #666;
-      top: 0;
-      left: 2ch;
+
+      display: inline-block;
+      margin-bottom: 1rem;
+      margin-left: 1.25rem;
       font-size: .75em;
+
+      color: #666;
     }
   }
 </style>
--- tests/components/Cell.spec.js
+++ tests/components/Cell.spec.js
@@ -10,4 +10,14 @@ describe('Cell', () => {
       .should('contain.text', cellConfig.letter)
       .and('contain.text', cellConfig.number)
   })
+
+  it('renders a blocked out cell when theres no letter', () => {
+    const cellConfig = { letter: '', number: 0 }
+    mount(Cell, { propsData: cellConfig })
+
+    cy.get('[data-testid=cell]').then((cell) => {
+      expect(cell.text().trim()).to.equal('')
+      expect(cell).to.have.class('blocked-out')
+    })
+  })
 })
